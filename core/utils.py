73b613283eed455c51dbde8a34a184670cfd11d6
Alexis <alexispina117@gmail.com>
2021-04-15 21:40:22 -0500

    Adding dynamo db functionality

---
+++ core/utils.py
@@ -0,0 +1,48 @@
+import os
+
+import requests
+
+
+def send_sms():
+    '''Sends an sms.'''
+    sms_api_key = os.environ['SMS_API_KEY']
+    phone_number = os.environ['PHONE_NUMBER']
+
+    headers = {"apikey": sms_api_key}
+
+    data = {
+        'message':'Este es un mensaje enviado desde una función lambda :)', 
+        'numbers':[phone_number],
+        'country_code':52,
+    }
+
+    request = requests.post(url = 'https://api.smsmasivos.com.mx/sms/send', data=data, headers=headers)
+
+def get_db_data(db_table):
+    '''If data does not exists (first call), we initialize it. '''
+    try:
+        response = db_table.get_item(Key={
+                'key': 1
+            })
+        data = response['Item']
+    except:
+        db_table.put_item(Item={
+                'key': 1,
+                'initial_amount': 100,
+                'last_buying_amount': 100,
+                'current_amount': 100,
+                'current_currency': 'USD',
+                'cumulative_earnings': 0
+            })
+        response = db_table.get_item(Key={
+                'key': 1
+            })
+        data = response['Item']
+
+    return {
+        'initial_amount': float(data['initial_amount']),
+        'last_buying_amount': float(data['last_buying_amount']),
+        'current_amount': float(data['current_amount']),
+        'current_currency': data['current_currency'],
+        'cumulative_earnings': float(data['cumulative_earnings'])
+    }
--- lambda_function.py
+++ lambda_function.py
@@ -1,20 +1,22 @@
 import json
+import os
+
+import boto3
 import requests
+    
+from core.utils import send_sms, get_db_data
 
 def lambda_handler(event, context):
-    # TODO implement
-    sms_api_key = os.environ['SMS_API_KEY']
     
-    headers = {"apikey": sms_api_key}
+    dynamodb = boto3.resource('dynamodb')
+    table_name = os.environ['DYNAMO_DB_TABLE']
+    table = dynamodb.Table(table_name)
+    
+    data = get_db_data(db_table=table)
 
-    data = {
-        'message':'Bienvenido a bitgoingAPP, tu código de verificación es: ', 
-        'numbers':['5581064181'],
-        'country_code':52,
-    }
-
-    request = requests.post(url = 'https://api.smsmasivos.com.mx/sms/send', data=data, headers=headers)
+    send_sms()
+    
     return {
         'statusCode': 200,
-        'body': json.dumps('Hello from Lambda!')
+        'body': json.dumps(data)
     }
